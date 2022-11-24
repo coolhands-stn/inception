beb36e857c8270035ebc6c2f252ada7e9137ad95
stnganyani <stnganyani@gmail.com>
2022-11-24 21:23:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,3 @@ video = st.file_uploader(label="upload video", type="mp4", key="video_upload_fil
 
 if(video is not None):
     st.text("video has been uploaded")
-    st.write('''
-    <video loop autoplay>
-        <source src={video} />
-    </video>
-     ''')
-
-
